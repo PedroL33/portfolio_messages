576c424491f5f42ec2cdf66dde9589a50fa4377c
peter <leepeter019@gmail.com>
2020-05-21 21:56:51 -0700

    Added methods to save messages

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ var cookieParser = require('cookie-parser');
 var indexRouter = require('./routes/index');
 
 var app = express();
+var port = process.env.PORT || 3000;
+
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', indexRouter);
-app.listen(3000);
+app.listen(port);
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var Message = require('../models/message');
+var { check, validationResult } = require('express-validator');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.json({msg: "Welcome"})
 });
+
+router.post('/', [
+  check('email').isEmail().withMessage("Invalid email."),
+  (req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        return res.status(422).json({
+            errors: errors.array()
+        })
+    }
+    var message = new Message({
+      message: req.body.message,
+      email: req.body.email
+    })
+    message.save((err, result) => {
+      if(err) {
+        return res.status(400).json({
+          message: "Something went wrong."
+        })
+      }else {
+        return res.status(200).json({
+          message: "Message recieved."
+        })
+      }
+    })
+  } 
+])
 
 module.exports = router;
