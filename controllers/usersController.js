fe0911e8211f17d5692cee081eb64daf5cdce962
peter <leepeter019@gmail.com>
2021-04-10 13:30:54 -0700

    Fixed unhandled promise rejection in login.

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,35 +4,41 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 exports.login = (req, res) => {
-  User.find({Username: req.body.username}, (err, results) => {
-    if(err) {      
-      return res.status(400).json({
-        msg: "Something went wrong."
-      })
-    }else if(!results.length) {
-      return res.status(400).json({
-        msg: "Authentication failed."
-      })
-    }else {
-      console.log(req.body.password, results[0].Password)
-      bcrypt.compare(req.body.password, results[0].Password, function(err, valid) {
-        if(err || !valid) {
-          res.status(400).json({
-            msg: "Authentication failed."
-          })
-        }else {
-          const token = jwt.sign({
-            exp: Math.floor(Date.now() / 1000) + (60 * 60),
-            data: req.body.username
-          }, process.env.JWT_SECRET);
-          res.status(200).json({
-            msg: "Authentication successful.",
-            token: token
-          })
-        }
-      })
-    }
-  })
+  try {
+    User.find({Username: req.body.username}, (err, results) => {
+      if(err) {      
+        return res.status(400).json({
+          msg: "Something went wrong."
+        })
+      }else if(!results.length) {
+        return res.status(400).json({
+          msg: "Authentication failed."
+        })
+      }else {
+        console.log(req.body.password, results[0].Password)
+        bcrypt.compare(req.body.password, results[0].Password, function(err, valid) {
+          if(err || !valid) {
+            res.status(400).json({
+              msg: "Authentication failed."
+            })
+          }else {
+            const token = jwt.sign({
+              exp: Math.floor(Date.now() / 1000) + (60 * 60),
+              data: req.body.username
+            }, process.env.JWT_SECRET);
+            res.status(200).json({
+              msg: "Authentication successful.",
+              token: token
+            })
+          }
+        })
+      }
+    })
+  }catch {
+    return res.status(400).json({
+      msg: "Authentication failed."
+    })
+  }
 }
 
 exports.createUser = (req, res) => {
